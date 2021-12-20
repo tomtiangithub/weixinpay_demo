cce0bff84e537af1fe409b45c11deeae78e74ba0
Your Name <you@example.com>
2021-12-20 21:24:05 +0800

    entity 06

--- src/main/java/com/meihong/entity/OrderInfo.java
+++ src/main/java/com/meihong/entity/OrderInfo.java
@@ -8,16 +8,10 @@ import lombok.Data;
 public class OrderInfo  extends BaseEntity{
 
     private String title;//订单标题
-
     private String orderNo;//商户订单编号
-
     private Long userId;//用户id
-
     private Long productId;//支付产品id
-
     private Integer totalFee;//订单金额(分)
-
     private String codeUrl;//订单二维码连接
-
     private String orderStatus;//订单状态
 }
