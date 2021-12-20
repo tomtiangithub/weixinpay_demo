238cd0bd870d515d048292a6f8b9f0535585d79a
tom <984803465@qq.com>
2021-12-21 00:01:16 +0800

    entity 15

--- src/main/java/com/meihong/controller/ProductController.java
+++ src/main/java/com/meihong/controller/ProductController.java
@@ -40,6 +40,7 @@ public class ProductController {
         System.out.println("分支更新44");
         System.out.println("分支更新55");
         System.out.println("分支更新42");
+        System.out.println("分支更新42");
         return R.ok().data("productList", list);
     }
 
