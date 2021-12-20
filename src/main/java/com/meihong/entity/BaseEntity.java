d5a66357835dddf374b2834967a6042eee1946d3
Your Name <you@example.com>
2021-12-20 21:20:09 +0800

    entity 05

--- src/main/java/com/meihong/entity/BaseEntity.java
+++ src/main/java/com/meihong/entity/BaseEntity.java
@@ -12,8 +12,6 @@ public class BaseEntity {
     //定义主键策略：跟随数据库的主键自增
     @TableId(value = "id", type = IdType.AUTO)
     private String id; //主键
-
     private Date createTime;//创建时间
-
     private Date updateTime;//更新时间
 }
